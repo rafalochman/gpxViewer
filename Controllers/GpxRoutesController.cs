67851087d3d111bd4a48e8285535793d54de6b63
rafalochman <rafalochman99@gmail.com>
2021-03-12 11:45:50 +0100

    remove unused Create methods

--- Controllers/GpxRoutesController.cs
+++ Controllers/GpxRoutesController.cs
@@ -55,28 +55,6 @@ namespace gpxViewer.Controllers
             return View(gpxData);
         }
 
-        // GET: GpxRoutes/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: GpxRoutes/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Name,Distance,Time,Elevation,SentDate,FilePath,MapUrl")] GpxRoute gpxRoute)
-        {
-            if (ModelState.IsValid)
-            {
-                db.GpxRoutes.Add(gpxRoute);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(gpxRoute);
-        }
 
         // GET: GpxRoutes/Edit/5
         public ActionResult Edit(int? id)
